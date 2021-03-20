2bacfcc7830e53f298943e1651e8d5f9418c42a6
Marktl <damarktl@edu.aau.at>
2021-03-20 14:27:31 +0100

    Create task2_3.py

---
+++ 01/task2_3.py
@@ -0,0 +1,52 @@
+def computeMeanMedianMode (fileName):
+    try:
+        ratings_file = open(fileName)
+    except OSError:
+        print('Could not open/read file: ', fileName)
+        return 
+    except FileNotFoundError:
+        print('Could not find file: ', fileName)
+        return 
+    
+    ratings = list()
+
+    #skip header line
+    try:
+        next(ratings_file)
+        for line in ratings_file:
+            ratings.append(line.strip().split(",")[2])
+    except:
+        print('Error while reading file: ', fileName)
+            
+    ratings_file.close()
+
+    stars = {};
+    for i in range(1,11):
+        stars[0.5*i] = 0
+    
+    sum = 0
+    for rating in ratings:
+        sum += float(rating)
+        stars[float(rating)] = stars[float(rating)]+1
+
+    if len(ratings) % 2 == 1:
+        median = ratings[len(ratings)/2]
+    else:
+        median = ratings[int((len(ratings)/2+len(ratings)/2+1)/2)]  
+
+    mode = 0.5
+    for key in stars:
+        if stars[key] > stars[mode]:
+            mode = key
+        
+    return (sum/len(ratings), median, mode)
+
+def main():
+    average, median, mode = computeMeanMedianMode('ml-latest-small/ratings.csv')
+    if type(average) == float:
+        print('Average: ', average)
+        print('Median: ', median)
+        print('Mode: ', mode)
+    
+if __name__ == "__main__":
+    main()
