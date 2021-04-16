18638111c0ce16fde55a24d0aa52dbd3a706378d
Marktl <damarktl@edu.aau.at>
2021-04-16 17:13:02 +0200

    Cast input immediately to int

--- 03/nearestNeighbor.py
+++ 03/nearestNeighbor.py
@@ -4,20 +4,20 @@ import math
 
 def showTitlesAndGenresOfUser():
     movieRatings = ratings.merge(movies)
-    print(movieRatings.loc[movieRatings['user_id'] == int(userId)].head(15))
+    print(movieRatings.loc[movieRatings['user_id'] == userId].head(15))
 
 def calcUserSimilarity():
     userSimilarity = pd.DataFrame(index = [userId], columns = users['user_id'])
     userAverages = userItems.mean(axis = 1)
-    moviesOfUser = userItems.loc[int(userId)].dropna().index
+    moviesOfUser = userItems.loc[userId].dropna().index
     for i in userSimilarity.columns:
         upperSum = 0
         lowerSum1 = 0
         lowerSum2 = 0
         for movieId in moviesOfUser:
             if userItems.loc[i][movieId] >= 0:
-                upperSum += (userItems.loc[int(userId)][movieId] - userAverages.loc[int(userId)]) * (userItems.loc[i][movieId] - userAverages.loc[i])
-                lowerSum1 += (userItems.loc[int(userId)][movieId] - userAverages.loc[int(userId)]) ** 2
+                upperSum += (userItems.loc[userId][movieId] - userAverages.loc[userId]) * (userItems.loc[i][movieId] - userAverages.loc[i])
+                lowerSum1 += (userItems.loc[userId][movieId] - userAverages.loc[userId]) ** 2
                 lowerSum2 += (userItems.loc[i][movieId] - userAverages.loc[i]) ** 2
         if lowerSum1 == 0 or lowerSum2 == 0:
             userSimilarity.loc[userId][i] = 0
@@ -34,8 +34,8 @@ ratings = pd.read_csv('./ml-1m/ratings.csv')
 
 # Get a user id
 while True:
-    userId = input('Enter a user id: ')
-    if (users['user_id'] == int(userId)).any():
+    userId = int(input('Enter a user id: '))
+    if (users['user_id'] == userId).any():
         break
     print('There is no such user id.')
 
